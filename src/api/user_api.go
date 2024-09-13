823fa2416ea192a3dd0efe0dc35f78d3525e9541
fanxingkun <fanxingkun@looki.ai>
2024-09-13 04:12:50 -0700

    BaseApi封装

--- src/api/user_api.go
+++ src/api/user_api.go
@@ -2,53 +2,27 @@ package api
 
 import (
 	"HANG-backend/src/service/dto"
-	"HANG-backend/src/utils"
-	"errors"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
-	"reflect"
 )
 
-type UserApi struct{}
+type UserApi struct {
+	BaseApi
+}
 
 func NewUserApi() UserApi {
-	return UserApi{}
+	return UserApi{
+		BaseApi: NewBaseApi(),
+	}
 }
 
-func (m UserApi) Login(ctx *gin.Context) {
+func (m UserApi) Login(c *gin.Context) {
 	var iUserLoginDTO dto.UserLoginDTO
-	errs := ctx.ShouldBind(&iUserLoginDTO)
-	if errs != nil {
-		Fail(ctx, ResponseJson{
-			Msg: parseValidateErrors(errs.(validator.ValidationErrors), &iUserLoginDTO).Error(),
-		})
+
+	if err := m.BuildRequest(BuildRequestOption{Ctx: c, DTO: &iUserLoginDTO}).GetError(); err != nil {
 		return
 	}
-	OK(ctx, ResponseJson{
+
+	m.OK(ResponseJson{
 		Data: iUserLoginDTO,
 	})
-}
-
-func parseValidateErrors(errs validator.ValidationErrors, target any) error {
-	var errResult error
-
-	// 通过反射获取指针指向元素的类型对象
-	fields := reflect.TypeOf(target).Elem()
-	for _, fieldErr := range errs {
-		field, _ := fields.FieldByName(fieldErr.Field())
-		errMessageTag := fmt.Sprintf("%s_err", fieldErr.Tag())
-		errMessage := field.Tag.Get(errMessageTag)
-		if errMessage == "" {
-			errMessage = field.Tag.Get("message")
-		}
-
-		if errMessage == "" {
-			errMessage = fmt.Sprintf("%s: %s Error", fieldErr.Field(), fieldErr.Tag())
-		}
-
-		errResult = utils.AppendError(errResult, errors.New(errMessage))
-	}
-
-	return errResult
 }
